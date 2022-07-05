a345cd9d52153bb6d6e3ed92c902dff2f7baa6dd
Devin Catuns <deecat10@gmail.com>
2022-07-05 04:38:11 -0400

    remove bundle analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,5 @@
 const { merge } = require("webpack-merge")
 const common = require("./webpack.common.js")
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = merge(common, {
@@ -32,10 +30,6 @@ module.exports = merge(common, {
         { from: "public/*.webmanifest", to: "[name][ext]" },
         { from: "public/*.css", to: "[name][ext]" },
       ],
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-      openAnalyzer: false,
     }),
   ],
 })
